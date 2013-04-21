d04b6f72920a3f4ba7c84d136d40e7c3960d1e08
Adam Hakowski <adam.hakowski@gmail.com>
2013-04-21 21:38:59 +0200

    Creat files and project

---
+++ ztt_ah/ztt_ah/coder.h
@@ -0,0 +1,26 @@
+/********************************************//**
+ * \file   airport.h
+ * \brief  Definicja obiektu koder.
+ * \author Adam Hakowski (adam.hakowski@gmail.com)
+ * \date   2013-04-21
+ ***********************************************/
+#ifndef CODER_H
+#define CODER_H
+/////////////////////////////////////////////////
+/*! \brief Koder.
+ *
+ *	Implementacja Koder.
+ *
+ */
+class Coder
+{
+	
+
+public:
+
+private:
+
+};
+/////////////////////////////////////////////////
+//***********************************************
+#endif /*CODER_H*/
